49bcf24b1076effb62125d204d08439aac80066b
Pranay Jain <jpranay688@gmail.com>
2021-12-31 00:11:52 +0530

    add desktop navbar & searchbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,49 @@
-const Navbar = () => {
-  return <div>Navbar</div>
+import { IoMdAdd, IoMdSearch } from "react-icons/io"
+import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+
+const Navbar = ({ searchTerm, setSearchTerm, user }) => {
+  // navigate to search component
+  const navigate = useNavigate()
+
+  // if user not logged in return
+  if (!user) return null
+
+  return (
+    <div className='flex gap-2 md:gap-5 w-full mt-5 pb-7'>
+      {/* Search bar */}
+      <div className='flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm'>
+        <IoMdSearch fontSize={21} className='ml-1' />
+        <input
+          type='text'
+          onChange={e => setSearchTerm(e.target.value)}
+          placeholder='Search pins...'
+          value={searchTerm}
+          onFocus={() => navigate("/search")}
+          className='w-full p-2 bg-white outline-none'
+        />
+      </div>
+
+      <div className='flex gap-3'>
+        {/* User Profile */}
+        <Link to={`user-profile/${user?._id}`} className='hidden md:block'>
+          <img
+            src={user?.image}
+            alt={user?.username || "user profile"}
+            className='w-14 h-12 rounded-lg'
+          />
+        </Link>
+
+        {/* Create Pin Button */}
+        <Link
+          to='create-pin'
+          className='bg-black text-white rounded-lg h-12 w-12 md:w-14 flex justify-center items-center'
+        >
+          <IoMdAdd />
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
--- frontend/src/containers/Pins.jsx
+++ frontend/src/containers/Pins.jsx
@@ -4,22 +4,32 @@ import { Routes } from "react-router-dom"
 import { Navbar, Search, Feed, PinDetails, CreatePin } from "../components"
 
 const Pins = ({ user }) => {
+  // search query
   const [searchTerm, setSearchTerm] = useState("")
 
   return (
     <div className='px-2 md:px-5'>
       <div className='bg-gray-50'>
-        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <Navbar
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          user={user && user}
+        />
       </div>
+
       <div className='h-full'>
+        {/* setting up routes */}
         <Routes>
           <Route path='/' element={<Feed />} />
           <Route path='/category/:categoryId' element={<Feed />} />
           <Route
             path='/pin-detail/:pinId'
-            element={<PinDetails user={user} />}
+            element={<PinDetails user={user && user} />}
           />
-          <Route path='/create-pin' element={<CreatePin user={user} />} />
+          <Route
+            path='/create-pin'
+            element={<CreatePin user={user && user} />}
+          />
           <Route
             path='/search'
             element={
