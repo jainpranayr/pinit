1818c372470840001ea1b4fa7c65fd9d6b582717
Pranay Jain <jpranay688@gmail.com>
2022-01-01 17:28:32 +0530

    fix sidebar for mobile devices and add category images

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -49,6 +49,11 @@ const Sidebar = ({ user, closeToggle }) => {
               onClick={handleCloseSidebar}
               key={category.name}
             >
+              <img
+                src={category?.image}
+                alt='category'
+                className='w-8 h-8 rounded-full shadow-sm'
+              />
               {category.name}
             </NavLink>
           ))}
--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -65,7 +65,7 @@ const Home = () => {
         {/* mobile menu toggle */}
         {toggleSideBar && (
           <div
-            className='fixed w-[48%] bg-white h-screen overflow-y-auto shadow-md z-10
+            className='fixed w-[50%] bg-white h-screen overflow-y-auto shadow-md z-10
         animate-slide-in'
           >
             <div className='absolute w-full flex justify-end items-center p-2'>
