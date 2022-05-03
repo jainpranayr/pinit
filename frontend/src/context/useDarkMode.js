f2350de2b047dd0afa827568ed82cf89e3aadfd9
Pranay Jain <jpranay688@gmail.com>
2022-05-04 01:09:47 +0530

    feat: setup dark mode context and dark mode toggle

---
+++ frontend/src/context/useDarkMode.js
@@ -0,0 +1,25 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+
+const DarkModeContext = createContext()
+
+export const DarkModeContextProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(true)
+
+  const handleDarkMode = () => {
+    localStorage.setItem('darkMode', !darkMode)
+    setDarkMode(!darkMode)
+  }
+
+  useEffect(() => {
+    const darkMode = localStorage.getItem('darkMode') === 'true'
+    setDarkMode(darkMode)
+  }, [])
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, handleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  )
+}
+
+export const useDarkMode = () => useContext(DarkModeContext)
